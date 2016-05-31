6821f91e5f3956e76085e3252bd7af808abf3454
shahar halutz <shahar.halutz@gmail.com>
2016-05-31 12:38:22 +0200

    comments

--- RNApp/index.ios.js
+++ RNApp/index.ios.js
@@ -2,3 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './app';
 
 AppRegistry.registerComponent('RNApp', () => App);
+
+// TBD: react-native-checkbox, and react-native-selectme cuase deprecation warnings of ReactNative.propTypes and ReactNative.Component
+//      replace with other modules
